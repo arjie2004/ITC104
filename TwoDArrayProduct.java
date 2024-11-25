9d89e7be69414ffc35aa0782d929457da074119d
arjie2004 <arjiedacut18@gmail.com>
2024-11-25 21:39:09 +0800

    Add files via upload

---
+++ TwoDArrayProduct.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TwoDArrayProduct {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        
+        
+        int[][] array = new int[rows][cols];
+        
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[i][j] = i * j;
+        }
+        
+        
+        System.out.println("The resulting 2D array is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}
